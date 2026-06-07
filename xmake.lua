86d045f327f021dd2c6ee74854eac1c8bb51eaad
holdxen <zhousir429@gmail.com>
2026-06-08 00:44:17 +0800

    fix

--- xmake.lua
+++ xmake.lua
@@ -376,7 +376,7 @@ package_end()
 -- 7. subversion (depends on all packages except serf)
 package("subversion")
     set_sourcedir(path.join(rootdir, "subversion"))
-    add_deps("zlib", "openssl", "apr", "libexpat", "apr-util")
+    add_deps("zlib", "openssl", "apr", "libexpat", "apr-util", "libxcrypt")
     on_install(function (package)
 
         os.vrunv("git", {"restore", "."}, {curdir = path.join(os.scriptdir(), "subversion")})
